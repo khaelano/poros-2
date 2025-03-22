30cce7febaaa5812c804273d2d30c7779e449d61
khaelano <khaelanoabroor@gmail.com>
2025-03-22 14:40:18 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Navbar() {
+  const navbarStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    background: "#282c34",
+    padding: "10px 20px",
+  };
+
+  const linkStyle = {
+    color: "white",
+    textDecoration: "none",
+    marginRight: "20px",
+  };
+
+  return (
+    <nav style={navbarStyle}>
+      <img src={logo} alt="logo" style={{ height: "40px" }} />
+      <div>
+        <a href="#" style={linkStyle}>
+          Home
+        </a>
+        <a href="#" style={linkStyle}>
+          About
+        </a>
+        <a href="#" style={linkStyle}>
+          Contact
+        </a>
+      </div>
+    </nav>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
